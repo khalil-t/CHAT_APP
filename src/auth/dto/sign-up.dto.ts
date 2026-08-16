20452a1741de0aa9bcbfb3584572aa029783ae2d
Touil Khalil <khaliltouil123s@gmail.com>
2026-08-16 18:23:53 +0100

    feat: add Auth (JWT)

---
+++ src/auth/dto/sign-up.dto.ts
@@ -0,0 +1,39 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  Matches,
+  maxLength,
+  MaxLength,
+  MinLength,
+  IsDate
+} from 'class-validator';
+
+
+export class SignUpDto{
+
+
+    @IsEmail()
+    @MaxLength(255)
+    @IsNotEmpty()
+    readonly email : string
+
+
+    @MinLength(8,{
+        message :  'password too short',
+    })
+    @MaxLength(20,{
+         message: 'password too long',
+    })
+     @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+     message: 'password too weak',
+     })
+    @IsNotEmpty()
+    readonly password: string;
+
+  @IsNotEmpty()
+  readonly passwordConfirm: string;
+
+@IsDate()
+  readonly created_at : Date
+}
+
